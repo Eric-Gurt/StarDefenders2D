0d3271966fb1eb4090f8f955d3dc9ad24de962a1
Booraz149 <81060329+Booraz149@users.noreply.github.com>
2021-04-12 18:11:31 +0200

    Flying Mech drops now crystal shards on death

--- game/entities/sdEnemyMech.js
+++ game/entities/sdEnemyMech.js
@@ -231,7 +231,7 @@ class sdEnemyMech extends sdEntity
 			this.remove();
 			
 */
-			this.MatterGlow( 0.01, 30, GSPEED );
+			//this.MatterGlow( 0.01, 30, GSPEED );
 			/*var x = this.x;
 			var y = this.y;
 			for ( var xx = -1; xx <= 1; xx++ )
@@ -610,6 +610,11 @@ class sdEnemyMech extends sdEntity
 		sdEnemyMech.mechs_counter--;
 		sdWorld.BasicEntityBreakEffect( this, 25, 3, 0.75, 0.75 );
 		//sdSound.PlaySound({ name:'crystal', x:this.x, y:this.y, volume:1 });
+		sdWorld.DropShards( this.x, this.y, 0, 0, 
+			Math.floor( Math.max( 0, this.matter / this.matter_max * 40 / sdWorld.crystal_shard_value * 0.5 ) ),
+			this.matter_max / 40
+		);
+
 	}
 	MeasureMatterCost()
 	{
