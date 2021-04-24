71e910aa7d10451cfcdf8668b37ddb77db77fa08
Booraz149 <81060329+Booraz149@users.noreply.github.com>
2021-04-24 19:59:00 +0200

    Include sdJunk under development tests category

--- game/client/sdShop.js
+++ game/client/sdShop.js
@@ -298,6 +298,7 @@ class sdShop
 			sdShop.options.push({ _class: 'sdSlug', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdEnemyMech', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdMatterContainer', matter_max:640 * 2 * 2, _category:'Development tests' });
+			sdShop.options.push({ _class: 'sdJunk', _category:'Development tests' });
 		}
 		
 		sdShop.options.push({ _class: 'sdArea', type:sdArea.TYPE_PREVENT_DAMAGE, size:256, _category:'Admin tools' });
