cea7c7563023d9e3ad2084f05a6f4657cf97f655
c86ec23b-fef1-4979-b2fa-b9adc351b8cc <87239823+c86ec23b-fef1-4979-b2fa-b9adc351b8cc@users.noreply.github.com>
2021-09-11 15:36:06 +0300

    Update sdShop.js

--- game/client/sdShop.js
+++ game/client/sdShop.js
@@ -430,6 +430,7 @@ class sdShop
 			sdShop.options.push({ _class: 'sdCrystal', tag: 'deep', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdCrystal', tag: 'deep', matter_max: 5120, _category:'Development tests' }); // Glowing one
 			sdShop.options.push({ _class: 'sdCrystal', tag: 'deep', matter_max: sdCrystal.anticrystal_value, _category:'Development tests' });
+			sdShop.options.push({ _class: 'sdCrystal', tag: 'deep', matter_max: 11000, _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdDrone', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdLost', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdGun', class:sdGun.CLASS_LOST_CONVERTER, _category:'Development tests' });
