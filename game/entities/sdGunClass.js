bb2980105ebeb11908050f14167a9953178a98a0
Molisson <81079603+Molisson@users.noreply.github.com>
2022-06-11 22:13:55 +0200

    Rayrifle Gun (reupload) (#94)
    * Rayrifle image
    
    * Rayrifle gun
    
    * Updated Rayrifle
    
    * Re-updated the rayrifle.
    
    Should be all fixed by now.

--- game/entities/sdGunClass.js
+++ game/entities/sdGunClass.js
@@ -3013,6 +3013,24 @@ class sdGunClass
 			projectile_properties: { _rail: true, time_left: 0, _damage: 1, color: '#ffffff'}
 		};
 
+		sdGun.classes[ sdGun.CLASS_RAYRIFLE = 94 ] =
+		{
+			image: sdWorld.CreateImageFromFile( 'rayrifle_tcorr' ),
+			sound: 'gun_rifle',
+			sound_volume: 1.2,
+			sound_pitch: 0.2,
+			title: 'Ray Rifle TCoRR',
+			slot: 2,
+			reload_time: 2.8,
+			muzzle_x: 7,
+			ammo_capacity: 16,
+			count: 1,
+			spread: 0.01, // 0.03
+			projectile_properties: { _damage: 18, color: '#afdfff', penetrating: true }, // Damage used to be 27, but it was too OP for beginners.
+			matter_cost: 160,
+			spawnable:false
+		};
+
 		// Add new gun classes above this line //
 		
 		let index_to_const = [];
