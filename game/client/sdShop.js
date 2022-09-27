a7d427b32942b461e669f7fb7a463b26f5943e07
Eric Gurt <41112429+Eric-Gurt@users.noreply.github.com>
2022-09-27 08:08:11 +0300

    Fix

--- game/client/sdShop.js
+++ game/client/sdShop.js
@@ -251,7 +251,7 @@ class sdShop
 		sdShop.options.push({ _class: 'sdCom', variation: 5, _category:'Base equipment', _min_build_tool_level:10 });
 		sdShop.options.push({ _class: 'sdCom', variation: 6, _category:'Base equipment', _min_build_tool_level:12 });
 		sdShop.options.push({ _class: 'sdCom', variation: 7, _category:'Base equipment', _min_build_tool_level:14 });
-		//sdShop.options.push({ _class: 'sdTeleport', _category:'Base equipment' });
+		sdShop.options.push({ _class: 'sdTeleport', _category:'Base equipment' });
 		sdShop.options.push({ _class: 'sdAntigravity', _category:'Base equipment' });
 		sdShop.options.push({ _class: 'sdLamp', _category:'Base equipment' });
 		sdShop.options.push({ _class: 'sdStorage', filter: 'saturate(0)', _category:'Base equipment' });
