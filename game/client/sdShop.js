5015f0ad2960ba6746e14ae0b047bf990b44273d
Davor Sardelić <davor.sardelic@gmail.com>
2023-03-25 11:40:59 +0100

    add Tzyrg device to dev tools / sdShop

--- game/client/sdShop.js
+++ game/client/sdShop.js
@@ -692,6 +692,7 @@ class sdShop
 			sdShop.options.push({ _class: 'sdJunk', type: 7, _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdJunk', type: 8, _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdCouncilMachine', _category:'Development tests' });
+			sdShop.options.push({ _class: 'sdTzyrgAbsorber', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdBadDog', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdShark', _category:'Development tests' });
 			sdShop.options.push({ _class: 'sdWorkbench', _category:'Development tests' });
