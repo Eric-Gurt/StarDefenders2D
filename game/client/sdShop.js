c2c29fadfba96816ad906dc85cd6b5988045bc6d
AloneGuitar <126790653+AloneGuitar@users.noreply.github.com>
2023-05-14 20:24:36 +0800

    sdFactionTools in Admin Tools

--- game/client/sdShop.js
+++ game/client/sdShop.js
@@ -657,6 +657,15 @@ class sdShop
 			//sdShop.options.push({ _class: 'sdDeepSleep', w:64, h:64, type:0, _category:'Admin tools' });
 			//sdShop.options.push({ _class: 'sdDeepSleep', w:64, h:64, type:1, _category:'Admin tools' });
 
+			sdShop.options.push({ _class: 'sdFactionTools', _category:'Admin tools' }); // Make easier to spawn sdCharacter.
+			sdShop.options.push({ _class: 'sdFactionTools', type:2, _category:'Admin tools' });
+			sdShop.options.push({ _class: 'sdFactionTools', type:3, _category:'Admin tools' });
+			sdShop.options.push({ _class: 'sdFactionTools', type:4, _category:'Admin tools' });
+			sdShop.options.push({ _class: 'sdFactionTools', type:5, _category:'Admin tools' });
+			sdShop.options.push({ _class: 'sdFactionTools', type:6, _category:'Admin tools' });
+			sdShop.options.push({ _class: 'sdFactionTools', type:7, _category:'Admin tools' });
+			sdShop.options.push({ _class: 'sdFactionTools', type:8, _category:'Admin tools' });
+
 			//let remover_sd_filter = sdWorld.CreateSDFilter();
 			//sdWorld.ReplaceColorInSDFilter_v2( remover_sd_filter, '#abcbf4', '#ff9292' );
 
