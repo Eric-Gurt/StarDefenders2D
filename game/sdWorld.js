afffce761e6630b302e1d4ccf87a203bdee72c58
Davor Sardelić <davor.sardelic@gmail.com>
2023-07-17 01:07:20 +0200

    Make large trees occasionally spawn on fresh world
    About 35% chance to spawn instead of small trees.

--- game/sdWorld.js
+++ game/sdWorld.js
@@ -958,7 +958,8 @@ class sdWorld
 
 					if ( Math.random() < 0.2 && !will_break_on_touch )
 					{
-						let tree = new sdGrass({ x:x + 16 / 2, y:y, filter:f, variation:( Math.random() < 0.2 ) ? sdGrass.VARIATION_TREE_BARREN : sdGrass.VARIATION_TREE });
+						let tree_variation = ( Math.random() < 0.35 ) ? ( ( Math.random() < 0.2 ) ? sdGrass.VARIATION_TREE_LARGE_BARREN : sdGrass.VARIATION_TREE_LARGE ) : ( ( Math.random() < 0.2 ) ? sdGrass.VARIATION_TREE_BARREN : sdGrass.VARIATION_TREE );
+						let tree = new sdGrass({ x:x + 16 / 2, y:y, filter:f, variation: tree_variation });
 						sdEntity.entities.push( tree );
 
 						plants.push( tree._net_id );
