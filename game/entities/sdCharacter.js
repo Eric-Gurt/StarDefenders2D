8393618ae9e277317f8a898222ccefa0db3269e0
AloneGuitar <126790653+AloneGuitar@users.noreply.github.com>
2023-08-31 13:51:42 +0800

    Add files via upload

--- game/entities/sdCharacter.js
+++ game/entities/sdCharacter.js
@@ -513,10 +513,10 @@ class sdCharacter extends sdEntity
 	
 	GetBleedEffect()
 	{
-		if ( this._voice.variant === 'whisperf' || this._voice.variant === 'croak' || this._voice.variant ==='m2' && this._voice.voice === 'hr' )
+		if ( this._voice.variant === 'whisperf' || this._voice.variant === 'croak' || this._voice.variant ==='m2' )
 		return sdEffect.TYPE_BLOOD_GREEN;
 		
-		if ( this._voice.variant === 'klatt3' || this._voice.variant === 'silence' || this._voice.variant ==='m2' && this._voice.voice !== 'hr' )
+		if ( this._voice.variant === 'klatt3' || this._voice.variant === 'silence' || this._voice.variant ==='erthal' || this._voice.variant ==='tzyrg' )
 		return sdEffect.TYPE_WALL_HIT;
 	
 		return sdEffect.TYPE_BLOOD;
