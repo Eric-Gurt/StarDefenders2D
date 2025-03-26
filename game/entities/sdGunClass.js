e5feca0749674f55098c90cccdddd4cc2b211476
c86ec23b-fef1-4979-b2fa-b9adc351b8cc <87239823+c86ec23b-fef1-4979-b2fa-b9adc351b8cc@users.noreply.github.com>
2025-03-26 13:39:47 +0200

    Update sdGunClass.js

--- game/entities/sdGunClass.js
+++ game/entities/sdGunClass.js
@@ -9923,7 +9923,7 @@ class sdGunClass
 										bullet_obj.model = 'stalker_target';
 										bullet_obj.color = sdEffect.default_explosion_color;
 										bullet_obj.time_left = Number.MAX_SAFE_INTEGER;
-										bullet_obj._hea = 300;
+										bullet_obj._hea = 100;
 								
 										sdEntity.entities.push( bullet_obj );
 									}
