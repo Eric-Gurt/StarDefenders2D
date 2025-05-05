918988a555f9cd86a9fb37c79e6468f7cf124d25
c86ec23b-fef1-4979-b2fa-b9adc351b8cc <87239823+c86ec23b-fef1-4979-b2fa-b9adc351b8cc@users.noreply.github.com>
2025-05-05 17:40:00 +0300

    Update sdGunClass.js

--- game/entities/sdGunClass.js
+++ game/entities/sdGunClass.js
@@ -10003,6 +10003,14 @@ class sdGunClass
 										{
 											sdWorld.SendEffect({ x:ent.x, y:ent.y, type:sdEffect.TYPE_TELEPORT });
 											sdSound.PlaySound({ name:'teleport', x:ent.x, y:ent.y, volume:0.5 });
+											
+											for( let i = 0; i < ent._inventory.length; i++ ) // Prevent loot from being stolen and disappearing
+											{
+												let item = ent._inventory[ i ];
+												
+												if ( i !== ent.gun_slot )
+												ent.DropWeapon( i );
+											}
 									
 											ent.remove();
 										}
