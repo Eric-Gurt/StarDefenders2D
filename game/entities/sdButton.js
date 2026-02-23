a38108fa77b673cd105f06256f6362d66e68392f
c86ec23b-fef1-4979-b2fa-b9adc351b8cc <87239823+c86ec23b-fef1-4979-b2fa-b9adc351b8cc@users.noreply.github.com>
2026-02-23 19:12:04 +0200

    Fix cable removal

--- game/entities/sdButton.js
+++ game/entities/sdButton.js
@@ -822,15 +822,14 @@ class sdButton extends sdEntity
                         if ( turret.kind !== sdTurret.KIND_AUTO_CABLE )
 						turret._owner = this._owner;
 
-                        if ( turret.kind === sdTurret.KIND_AUTO_CABLE && vv ) // Disconnect all cables in case of signal flipping node
+                        if ( turret.kind === sdTurret.KIND_AUTO_CABLE && vv ) // Disconnect all cables that were created by it in case of signal flipping node
                         {
-                            const cables = sdCable.cables_per_entity.get( turret );
-                            for ( const cable of cables )
+                            for ( const cable of turret._built_cables )
                             {
-                                if ( !cable._is_being_removed )
-                                if ( !cable.p.is_static || !cable.c.is_static )
+                                if ( cable && !cable._is_being_removed )
                                 cable.remove();
                             }
+                            turret._built_cables = [];
                         }
 					}
 					else
