827145c9e48dfa86bc4911020bca626c08fb90e3
c86ec23b-fef1-4979-b2fa-b9adc351b8cc <87239823+c86ec23b-fef1-4979-b2fa-b9adc351b8cc@users.noreply.github.com>
2026-03-10 07:33:20 +0200

    minigun

--- game/entities/sdGunClass.js
+++ game/entities/sdGunClass.js
@@ -11143,6 +11143,50 @@ class sdGunClass
                 '#00ffff', 15, 'glow 3' ),
                 '#adffff', 15, 'glow 4' ) )
 		};
+        sdGun.classes[ sdGun.CLASS_SD_MINIGUN = 162 ] = // Sprite by LazyRain
+		{
+			image: sdWorld.CreateImageFromFile( 'sd_minigun' ),
+			title: 'Minigun SD-134',
+			slot: 2,
+			sound: 'gun_pistol',
+			sound_pitch: 0.85,
+			sound_volume: 1,
+			reload_time: 2,
+			muzzle_x: 11,
+			ammo_capacity: -1,
+			count: 1,
+			spread: 0.05,
+			spawnable: false,
+			projectile_properties: { _damage: 25, _dirt_mult: -0.5 },
+			projectile_properties_dynamic: ( gun )=>{ 
+				
+				let obj = { _dirt_mult: -0.5 };
+				obj._knock_scale = 0.01 * 8 * gun.extra[ ID_DAMAGE_MULT ];
+				obj._damage = gun.extra[ ID_DAMAGE_VALUE ]; // Damage value is set onMade
+				obj._damage *= gun.extra[ ID_DAMAGE_MULT ];
+				obj._knock_scale *= gun.extra[ ID_RECOIL_SCALE ];
+				
+				if ( gun.extra[ ID_PROJECTILE_COLOR ] )
+				obj.color = gun.extra[ ID_PROJECTILE_COLOR ];
+
+				return obj;
+			},
+			onMade: ( gun, params )=> // Should not make new entities, assume gun might be instantly removed once made
+			{
+				if ( !gun.extra )
+				{
+					gun.extra = [];
+					gun.extra[ ID_DAMAGE_MULT ] = 1;
+					//gun.extra[ ID_FIRE_RATE ] = 1;
+					gun.extra[ ID_RECOIL_SCALE ] = 1;
+					//gun.extra[ ID_SLOT ] = 1;
+					gun.extra[ ID_DAMAGE_VALUE ] = 25; // Damage value of the bullet, needs to be set here so it can be seen in weapon bench stats
+				}
+			},
+			upgrades: AddGunDefaultUpgrades( AddRecolorsFromColorAndCost
+				( [], '#cc841a', 15, ) )
+
+		};
 		// Add new gun classes above this line //
 
 		let index_to_const = [];
