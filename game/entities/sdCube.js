adbc94b2565b727fde87acb0bbc847e91d6a0b42
c86ec23b-fef1-4979-b2fa-b9adc351b8cc <87239823+c86ec23b-fef1-4979-b2fa-b9adc351b8cc@users.noreply.github.com>
2026-03-16 16:23:40 +0200

    Update sdCube.js

--- game/entities/sdCube.js
+++ game/entities/sdCube.js
@@ -892,10 +892,6 @@ class sdCube extends sdEntity
 					
 					}, 500 );
 				}
-                if ( this.kind === sdCube.KIND_RED )
-                {
-                    sdEntity.Create( sdRift, { type:sdRift.TYPE_CUBE_PORTAL, x:this.x, y:this.y } );
-                }
 			}
 			this.remove();
 		}
@@ -1132,6 +1128,9 @@ class sdCube extends sdEntity
 					
 					if ( this._boss_death_pings_left === 0 )
 					{
+                        if ( this.kind === sdCube.KIND_RED )
+                        sdEntity.Create( sdRift, { type:sdRift.TYPE_CUBE_PORTAL, x:this.x, y:this.y } );
+
 						for ( let t = 0; t < 5; t++ )
 						{
 							setTimeout( ()=>{
@@ -1175,8 +1174,7 @@ class sdCube extends sdEntity
 					}
 				}
 			}
-			
-			
+
 			if ( this.kind === sdCube.KIND_MATTER_STEALER )
 			this.HungryMatterGlow( 0.01, 100, 0.25 );
 			else
